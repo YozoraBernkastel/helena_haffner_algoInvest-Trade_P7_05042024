a56b01877dfa63899580c46fdeb84861256bf90c
Featherine <helaena.ha2fner@gmail.com>
2024-04-18 14:38:13 +0200

    #02 optimzed algo done

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from optimized import optimized_entry_point
 
 def main():
     entry_point()
-    optimized_entry_point()
+    first_file_path = "./input/dataset1_Python+P7.csv"
+    second_file_path = "./input/dataset2_Python+P7.csv"
+    optimized_entry_point(first_file_path)
+    optimized_entry_point(second_file_path)
 
 
 if __name__ == '__main__':
--- optimized.py
+++ optimized.py
@@ -8,30 +8,46 @@ def opti_format_data(file_path):
     data = read_csv(file_path)
     new_data = {}
     for d in data:
-        if float(d[1]) > 1.0 and float(d[2]) > 1.0:
-            new_data[d[0]] = [d[1], convert_percent_in_euro(float(d[1]), float(d[2]))]
+        if float(d[1]) > 0.01 and float(d[2]) > 0.01:
+            new_data[d[0]] = [float(d[1]), convert_percent_in_euro(float(d[1]), float(d[2])), float(d[2])]
 
-    return sorted(new_data.items(), key=lambda x: x[1][1], reverse=True)
-
-
-def compute_all_combinations(data: dict, r: int):
-    all_comb = list(combinations(data, r))
-    print(all_comb)
+    return (sorted(new_data.items(), key=lambda x: x[1][2], reverse=True),
+            sorted(new_data.items(), key=lambda x: x[1][1], reverse=True))
 
 
-def optimized_entry_point():
+def compute_sequence(data):
+    actions = []
+    total_profit = [0, 0.0, 0.0]
+
+    for d in data:
+        if total_profit[1] + d[1][0] > 500:
+            continue
+        actions.append(d[0])
+        total_profit[0] += 1
+        total_profit[1] += d[1][0]
+        total_profit[2] += d[1][1]
+
+    return actions, total_profit
+
+
+def compare_sequences(ratio_sequence: list, profit_sequence: list) -> None:
+    ratio_actions, ratio_total = compute_sequence(ratio_sequence)
+    profit_actions, profit_total = compute_sequence(profit_sequence)
+
+    best_total = ratio_total if ratio_total[2] > profit_total[2] else profit_total
+    best_sequence = ratio_actions if ratio_total[2] > profit_total[2] else profit_actions
+    print(f"Pour gagner {best_total[2]}€, dépensez {best_total[1]} Achetez les {best_total[0]} actions :")
+    for action in best_sequence:
+        print(f"   - {action}")
+
+
+
+def optimized_entry_point(file_path: str) -> None:
     start = time.time()
-    print("opti")
-    first_file_path = "./input/dataset1_Python+P7.csv"
-    data = opti_format_data(first_file_path)
-    print(data)
-
-    # best_profit = BestProfit()
-
-    # print(f"Meilleure combinaison -> {best_profit.combination}")
-    # print(f"Prix total de la combinaison -> {best_profit.total_price}")
-    # print(f"Profits sur deux ans -> {best_profit.value}")
+    print("############### opti ################\n")
+    ratio_data, profit_data = opti_format_data(file_path)
+    compare_sequences(ratio_data, profit_data)
 
     end = time.time()
 
-    print(f"Optimized Algo execution time -> { end - start}")
+    print(f"\nOptimized Algo execution time -> {end - start}")
